7dad91ef522df3e865c48ac713ac9db245710c21
Alejandro Aguilera garcia <aaguiler@c3r6s5.42malaga.com>
2022-10-05 18:03:08 +0200

    Add files

---
+++ ex02/Acount.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Acount.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 17:53:38 by aaguiler          #+#    #+#             */
+/*   Updated: 2022/10/05 18:01:40 by aaguiler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(){
+
+}
+
+Account::~Account(){
+	
+}
+
+Account::Account(int initial_deposit){
+	(void)initial_deposit;
+}
+
+static int	Account::getNbAccounts(void){
+
+}
+
+static int	Account::getTotalAmount(void){
+
+}
+
+static int Account::getNbDeposits(void){
+
+}
+
+static int	Account::getNbWithdrawals(void){
+
+}
+
+static void	Account::displayAccountsInfos(void){
+
+}
+
+void	Account::makeDeposit(int deposit){
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal){
+
+}
+
+int	Account::checkAmount(void){
+
+}
+
+void	Account::displayStatus(void){
+
+}
+
+static void	Account::_displayTimestamp(void){
+
+}
